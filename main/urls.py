531be921e4426187014a39f22de5c0d4e7bbc718
Keiji Shiba <12021270+cenerion@users.noreply.github.com>
2024-06-10 21:34:26 +0000

    created fiews and templates fofor uczen

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from . import views
+from main import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("uczniowie/list", views.UczniowieListView.as_view(), name='uczniowie'),
+    path('uczniowie/create', views.UcznCreateView.as_view(), name='create ucz'),
+    path('uczniowie/update/<pk>', views.UczniowieUpdate.as_view(), name='ucz update'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.forms import BaseModelForm
+from django.views.generic import ListView, CreateView, UpdateView
+from django.forms.widgets import DateInput
+from django.urls import reverse, reverse_lazy
+
+from main.models import Uczen
+
 
 def index(request:HttpRequest) -> HttpResponse:
     return HttpResponse("hi")
+
+class UczniowieListView(ListView):
+    model=Uczen
+    context_object_name = 'students'
+    
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'lista uczniów'
+        return context
+
+    
+class UcznCreateView(CreateView):
+    model = Uczen
+    template_name_suffix = '_create_form'
+    fields = ['imie', 'nazwisko', 'data_ur',
+              'plec', 'klasa', 'adres_zam']
+    success_url = reverse_lazy('uczniowie')
+
+    def get_form(self, form_class: type[BaseModelForm] | None = None) -> BaseModelForm:
+        form = super().get_form(form_class)
+        form.fields['data_ur'].widget = DateInput(attrs={'type':'date'})
+        return form
+
+
+class UczniowieUpdate(UpdateView):
+    model = Uczen
+    template_name_suffix = '_update_form'
+    fields = ['imie', 'nazwisko', 'data_ur',
+              'plec', 'klasa', 'adres_zam']
+    success_url = reverse_lazy('uczniowie')
+
+    def get_form(self, form_class: BaseModelForm | None = None) -> BaseModelForm:
+        form = super().get_form(form_class)
+        form.fields['data_ur'].widget = DateInput(attrs={'type':'date'})
+        return form
+    
