790bd2f2ea4661391e18bc39c056288dcfabf9e5
Keiji Shiba <12021270+cenerion@users.noreply.github.com>
2024-06-10 14:26:14 +0000

    defined models for school data

--- main/models.py
+++ main/models.py
@@ -1,19 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+
 class Nauczyciel(models.Model):
     imie = models.CharField(max_length=50, null=False)
     nazwisko = models.CharField(max_length=100, null=False)
-    pesel = models.CharField(max_length=11, null=False)
+    data_ur = models.DateField()
+    plec = models.Choices(GENDER_CHOICES)
 
-class Uczen(models.Model):
-    ...
-
-class Ocena(models.Model):
-    ...
 
 class Klasa(models.Model):
-    ...
+    profil = models.CharField(max_length=50, null=False)
+    wychowawca = models.ForeignKey(Nauczyciel, on_delete=models.SET_NULL)
+    symbol = models.CharField(max_length=5, null=False)
+
+
+class Uczen(models.Model):
+    imie = models.CharField(max_length=50, null=False)
+    nazwisko = models.CharField(max_length=100, null=False)
+    data_ur = models.DateField()
+    plec = models.Choices(GENDER_CHOICES)
+    klasa_F = models.ForeignKey(Klasa, on_delete=models.SET_NULL)
+    adres_zam = models.CharField()
+
 
 class Przedmiot(models.Model):
-    ...
+    nazwa = models.CharField(max_length=50, null=False)
+
+
+class Ocena(models.Model):
+    uczen = models.ForeignKey(Uczen)
+    nauczyciel = models.ForeignKey(Nauczyciel)
+    przedmiot = models.ForeignKey(Przedmiot)
+    wartosc = models.FloatField()
+    nazwa = models.CharField(max_length=50, null=False)
+    data_wyst = models.DateField()
